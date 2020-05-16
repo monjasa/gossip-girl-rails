d2ae4f54139840d91a16810a3279d46dbe239dfc
Monjasa <monja.nya@gmail.com>
2020-05-17 00:38:55 +0300

    v0.0.7 - Enhance Locales and UI/UX

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -30,7 +30,7 @@ class TweetsController < ApplicationController
 
     respond_to do |format|
       if @tweet.save
-        format.html { redirect_to root_path, notice: 'Tweet was successfully created.' }
+        format.html { redirect_to root_path, notice: t('.notice') }
         format.json { render :show, status: :created, location: @tweet }
       else
         format.html { render :new }
@@ -44,7 +44,7 @@ class TweetsController < ApplicationController
   def update
     respond_to do |format|
       if @tweet.update(tweet_params)
-        format.html { redirect_to @tweet, notice: 'Tweet was successfully updated.' }
+        format.html { redirect_to @tweet, notice: t('.notice') }
         format.json { render :show, status: :ok, location: @tweet }
       else
         format.html { render :edit }
@@ -58,7 +58,7 @@ class TweetsController < ApplicationController
   def destroy
     @tweet.destroy
     respond_to do |format|
-      format.html { redirect_to tweets_url, notice: 'Tweet was successfully destroyed.' }
+      format.html { redirect_to tweets_url, notice: t('.notice') }
       format.json { head :no_content }
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+
   scope (":locale"), locale: /#{I18n.available_locales.join("|")}/ do
     devise_for :users
     resources :tweets
     root 'tweets#index'
   end
+
 end
